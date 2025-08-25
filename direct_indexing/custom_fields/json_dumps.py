340255edef3f6b4d7e395a6b8e6af76ae6221be3
Sylvan Ridderinkhof <sylvanridderinkhof@gmail.com>
2025-08-25 15:38:04 +0200

    fix: json transaction has list of single value date

--- direct_indexing/custom_fields/json_dumps.py
+++ direct_indexing/custom_fields/json_dumps.py
@@ -36,6 +36,7 @@ def add_json_dumps(data):
         for field in JSON_FIELDS:
             if field not in activity:
                 continue
+            activity[field] = _json_dump_transaction_fix(activity[field], field)
             if isinstance(activity[field], list):
                 _json_dump_list(activity, field)
             else:
@@ -43,6 +44,18 @@ def add_json_dumps(data):
                     activity[f'json.{field}'] = json.dumps(activity[field])
                 except Exception as e:
                     logging.error(f"Error serializing {field}: type: {type(e)} stack: {e}")
+
+
+# JSON dump transaction fix, as the date is a list of one element, but the field is a string.
+def _json_dump_transaction_fix(subfield, field):
+    if field == 'transaction':
+        for t in subfield:
+            td = t['transaction-date']
+            if isinstance(td, list):
+                if len(td) > 1:
+                    logging.error(f"Transaction date is a list with more than one element: {td}")
+                t['transaction-date'] = td[0]
+    return subfield
 
 
 def _json_dump_list(activity, field):
